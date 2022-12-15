9fca64d83c6392fada554cc4d26841b162dfe163
karimSATPRO <107763973+karimSATPRO@users.noreply.github.com>
2022-12-15 20:35:12 +0100

    Add files via upload

---
+++ IPTV_List_m3u_DECODEWin.bat
@@ -0,0 +1,2 @@
+Python_Portable\python-3.8.9.amd64\python.exe Python_Portable/IPTV_List_m3u_DECODEWin.py
+
---
+++ Scraping Proxy.bat
@@ -0,0 +1,5 @@
+Python_Portable\python-3.8.9.amd64\python.exe Python_Portable/proxyScraper.py --proxy http --output proxies/sekryoga_http_proxy.txt
+Python_Portable\python-3.8.9.amd64\python.exe Python_Portable/proxyScraper.py --proxy https --output proxies/sekryoga_https_proxy.txt
+Python_Portable\python-3.8.9.amd64\python.exe Python_Portable/proxyScraper.py --proxy socks --output proxies/sekryoga_socks_proxy.txt
+Python_Portable\python-3.8.9.amd64\python.exe Python_Portable/proxyScraper.py --proxy socks4 --output proxies/sekryoga_socks4_proxy.txt
+Python_Portable\python-3.8.9.amd64\python.exe Python_Portable/proxyScraper.py --proxy socks5 --output proxies/sekryoga_socks4_proxy.txt
